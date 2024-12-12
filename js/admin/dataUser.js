9e73bbfe1ce0e1742142bcda16c487865265a0a0
LalaMunn <nhituyet20042008@gmail.com>
2024-12-13 00:05:32 +0700

    data

--- js/admin/dataUser.js
+++ js/admin/dataUser.js
@@ -10,4 +10,64 @@ const userArr = [
     true,
     true
   ),
+
+  new User(
+    "tranngocanh",
+    "12345anh",
+    "anhanh123@gmail.com",
+    "0909876567",
+    "Trần Ngọc Ánh",
+    "Hồ Chí Minh, Việt Nam",
+
+    true,
+    true
+  ),
+
+  new User(
+    "nhuluk",
+    "nhunhu0909",
+    "hnhuluk123@gmail.com",
+    "0123768493",
+    "H Như Lưk",
+    "Đắk Lắk, Việt Nam",
+
+    false,
+    true
+  ),
+
+  new User(
+    "phamtruong",
+    "6789pt",
+    "phamtruong123@gmail.com",
+    "0938573922",
+    "Trương Phạm",
+    "Hồ Chí Minh, Việt Nam",
+
+    false,
+    true
+  ),
+
+  new User(
+    "ayiaaa",
+    "abc345",
+    "ayi456@gmail.com",
+    "0123456789",
+    "A Yi Khải Ca",
+    "Cà Mau, Việt Nam",
+
+    false,
+    true
+  ),
+
+  new User(
+    "nhituyet",
+    "20042008",
+    "nhituyet20042008@gmail.com",
+    "0917008071",
+    "Nguyễn Tuyết Nhi",
+    "Bạc Liêu, Việt Nam",
+
+    false,
+    true
+  ),
 ];
